5af29a7d99d9683a4a1bef7e9306350d2d78756b
pc-aide <36453201+pc-aide@users.noreply.github.com>
2020-04-05 15:24:09 -0400

    Update Draft_Install_Setup.ps1

--- Draft_Install_Setup.ps1
+++ Draft_Install_Setup.ps1
@@ -4,7 +4,8 @@
 ----------------------------------------------------------------------------------------
 #>
 
-#Install
+#https://imgur.com/5RbremG
+
 $PWD = "..."
 Start-Process .\setup.exe -ArgumentList "/configurationFile=.\Install_Configuration.ini `
   /AGTSVCPASSWORD=$pwd  /sqlSvcPassword=$pwd /RSSVCPASSWORD=$PWD"
